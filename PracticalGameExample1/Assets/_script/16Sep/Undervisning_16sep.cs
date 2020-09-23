6f097191d83ec447432d243f698e0943e613a721
AkeBjork <ake.jobb@gmail.com>
2020-09-23 06:18:07 +0200

    testing

---
+++ PracticalGameExample1/Assets/_script/16Sep/Undervisning_16sep.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Undervisning_16sep : MonoBehaviour
+{
+    private int tall1 = 5;
+    private float tall2 = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        MinMetode(2f);
+        
+
+    }
+
+    float MinMetode(int verdi2)
+    {
+
+        //print(verdi);
+        return verdi2;
+    }
+
+    float MinMetode(float verdi3)
+    {
+
+        //print(verdi);
+        return verdi3;
+    }
+
+ 
+}
